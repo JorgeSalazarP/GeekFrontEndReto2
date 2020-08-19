2a6e81b6572800affae7914b089db3ebab76ab23
JorgeSalazarP <j-salazarp@hotmail.com>
2020-08-19 20:38:32 +0200

    programando el carrito

--- js/main.js
+++ js/main.js
@@ -6,70 +6,66 @@
     const soltar_productos = document.getElementById('carrito');
     let img_seleccionada="";
     let articulo ="";
-    let cantidad_0=100;
-    let cantidad_1=100;
-    let cantidad_2=100;
-    let cantidad_3=100;
-    let cantidad_4=100;
-    let cantidad_5=100;
+    let cantidad_articulo="";
+    let precio_articulo="";
+    let total_compra=0;
 
-   
     let frutas = [
         {   
             id: 0,
             content: 'Kiwi verde al peso (peso aproximado de la unidad 130g).',
             price: 3.89,
-            imagen:'img/KIWIS.png'
+            image:'img/KIWIS.png'
         },
         {
             id: 1,
             content: 'Melocotón selección al peso (peso aproximado de la unidad 150g).',
             price: 3.95,
-            imagen:'img/melocotones.png'
+            image:'img/melocotones.png'
         },
         {
             id: 2,
             content: 'Melón piel de sapo pieza 3,5kg peso aproximado.',
             price: 3.47,
-            imagen:'img/melon.png'
+            image:'img/melon.png'
         },
         {
             id: 3,
             content: 'Manzana golden al peso (peso aproximado de la unidad 200g).',
             price: 1.75,
-            imagen:'img/manzanas.png'
+            image:'img/manzanas.png'
         },
         {
             id: 4,
             content: 'Paraguayo al peso (peso aproximado de la unidad 130g)',
             price: 1.69,
-            imagen:'img/paraguayos.png'
+            image:'img/paraguayos.png'
         },
         {
             id: 5,
             content: 'Sandía cuarto pieza 1,5 kg peso aproximado',
             price:0.99,
-            imagen:'img/sandia.png'
+            image:'img/sandia.png'
         },
     ];
 
-   
+    
     for (let valor of frutas) {
         
         contenido.innerHTML += `
         <article class="col-md-4" id="articulo_tarjeta${valor.id}" >
 
         <div class="card" style="width: 18rem;">
-            <img src=${valor.imagen} id="tarjeta${valor.id}" class="card-img-top draggable="true">
+            <img src=${valor.image} id="tarjeta${valor.id}" class="card-img-top draggable="true">
             <div class="card-body">
-                <span class="precio">${valor.price}€/Kg</span>
+                <span id="precio_tarjeta${valor.id}">${valor.price}</span><span>€/Kg</span>
                 <p class="card-text">${valor.content}</p>
                 <div class="cantidad">
                     <button class="btn" id="restar_cantidad_${valor.id}">-</button>
-                    <span id="mostrar_cantidad${valor.id}">100</span>     
+                    <span id="mostrar_cantidad_tarjeta${valor.id}">100</span><span>g</span>
                     <button class="btn" id="sumar_cantidad_${valor.id}">+</button>
                 </div>
-                <p>Arrastrar al carrito de compra</p>
+                <p>Arrastrar la imagen al carrito de compra</p>
             </div>
         </div>
         
@@ -81,27 +77,36 @@
  
     document.addEventListener('dragstart',(e)=>{
 
-       
         img_seleccionada = document.getElementById(e.target.id);
         articulo = document.querySelector('#articulo_'+ e.target.id);
-      
-        
+        cantidad_articulo= document.querySelector('#mostrar_cantidad_'+e.target.id);
+        precio_articulo= document.querySelector('#precio_'+e.target.id);
         
     });
 
 
     document.addEventListener('click',(e)=>{
 
-        let elemento_pulsado = e.target.id;
-        let nombre_idboton = "sumar_cantidad_";
-        let pulsado_boton_sumar = elemento_pulsado.indexOf(nombre_idboton);
-
+        const elemento_pulsado = e.target.id;
+        const nombre_idboton = "sumar_cantidad_";
+        const pulsado_boton_sumar = elemento_pulsado.indexOf(nombre_idboton);
+        const nombre_idboton2 = "restar_cantidad_";
+        const pulsado_boton_restar = elemento_pulsado.indexOf(nombre_idboton2);
+        
         if (pulsado_boton_sumar !== -1){
-            console.log("La palabra está en la posición ");
-            let ultima=elemento_pulsado[elemento_pulsado.length-1];
+            
+            const ultima=elemento_pulsado[elemento_pulsado.length-1];
+            let numero_veces=parseInt(document.querySelector('#mostrar_cantidad_tarjeta'+ultima).textContent);
+            numero_veces+=50;
+            document.querySelector('#mostrar_cantidad_tarjeta'+ultima).innerHTML=numero_veces;
 
+        }
+        else if(pulsado_boton_restar !== -1){
 
-
+            const ultima=elemento_pulsado[elemento_pulsado.length-1];
+            let numero_veces=parseInt(document.querySelector('#mostrar_cantidad_tarjeta'+ultima).textContent);
+            numero_veces===100 ? numero_veces=100 : numero_veces-=50;
+            document.querySelector('#mostrar_cantidad_tarjeta'+ultima).innerHTML=numero_veces;
 
         }
         
@@ -119,180 +124,37 @@
     soltar_productos.addEventListener('drop',(e)=>{
         e.preventDefault();        
         soltar_productos.appendChild(img_seleccionada);
-        const cantidad = document.querySelector('#carrito article');
+        soltar_productos.appendChild(cantidad_articulo);
+        let simbolo_multiplicar = document.createElement("span");
+        simbolo_multiplicar.innerHTML=" X ";
+        soltar_productos.appendChild(simbolo_multiplicar);
+        soltar_productos.appendChild(precio_articulo);
+        let simbolo_igual = document.createElement("span");
+        simbolo_igual.innerHTML=" = ";
+        soltar_productos.appendChild(simbolo_igual);
 
-        cantidad.innerHTML=`3.95€/kg x 100 = 395€`;
+        let total = document.createElement("span");
+        let operacion=(parseFloat((cantidad_articulo.textContent)/1000)*parseFloat(precio_articulo.textContent)).toFixed(2);
+        
+        total.innerHTML=operacion+'€';
+        soltar_productos.appendChild(total);
+        total_compra+=operacion;
+        console.log(total_compra);
+        //soltar_productos.innerHTML=`<i class="fa fa-trash"></i>`;
         
 
-        //console.log(cantidad); 
-        //3.95€/kg x 100 = 395€
+
+
+
+
+
         articulo.style.display="none";
-
-      
-         
+        
     });
 
 
     
-/*
 
-    const boton_sumar_0=document.querySelector('.sumar_cantidad_0');
-    const boton_sumar_1=document.querySelector('.sumar_cantidad_1');
-    const boton_sumar_2=document.querySelector('.sumar_cantidad_2');
-    const boton_sumar_3=document.querySelector('.sumar_cantidad_3');
-    const boton_sumar_4=document.querySelector('.sumar_cantidad_4');
-    const boton_sumar_5=document.querySelector('.sumar_cantidad_5');
-
-    boton_sumar_0.addEventListener('click',()=>{
-        
-        cantidad_0+=50;
-        document.querySelector('#mostrar_cantidad0').innerHTML=cantidad_0;
-        
-    });
-
-    boton_sumar_1.addEventListener('click',()=>{
-
-        cantidad_1+=50;
-        document.querySelector('#mostrar_cantidad1').innerHTML=cantidad_1;
-        
-    });
-
-    boton_sumar_2.addEventListener('click',()=>{
-
-        cantidad_2+=50;
-        document.querySelector('#mostrar_cantidad2').innerHTML=cantidad_2;
-        
-    });
-
-    boton_sumar_3.addEventListener('click',()=>{
-
-        cantidad_3+=50;
-        document.querySelector('#mostrar_cantidad3').innerHTML=cantidad_3;
-        
-    });
-
-
-    boton_sumar_4.addEventListener('click',()=>{
-
-        cantidad_4+=50;
-        document.querySelector('#mostrar_cantidad4').innerHTML=cantidad_4;
-        
-    });
-    boton_sumar_5.addEventListener('click',()=>{
-
-        cantidad_5+=50;
-        document.querySelector('#mostrar_cantidad5').innerHTML=cantidad_5;
-        
-    });
-
-    const boton_restar_0=document.querySelector('.restar_cantidad_0');
-    const boton_restar_1=document.querySelector('.restar_cantidad_1');
-    const boton_restar_2=document.querySelector('.restar_cantidad_2');
-    const boton_restar_3=document.querySelector('.restar_cantidad_3');
-    const boton_restar_4=document.querySelector('.restar_cantidad_4');
-    const boton_restar_5=document.querySelector('.restar_cantidad_5');
-
-    boton_restar_0.addEventListener('click',()=>{
-
-        if (cantidad_0===100) {
-
-            cantidad_0=100;
-            
-        }
-        else{
-            cantidad_0-=50;
-            document.querySelector('#mostrar_cantidad0').innerHTML=cantidad_0;
-        }
-        
-        
-
-    });
-    
-    boton_restar_1.addEventListener('click',()=>{
-
-        if (cantidad_1===100) {
-
-            cantidad_1=100;
-            
-        }
-        else{
-            cantidad_1-=50;
-            document.querySelector('#mostrar_cantidad1').innerHTML=cantidad_1;
-        
-        }
-        
-        
-
-    });
-
-    boton_restar_2.addEventListener('click',()=>{
-
-        
-        if (cantidad_2===100) {
-
-            cantidad_2=100;
-            
-        }
-        else{
-            cantidad_2-=50;
-            document.querySelector('#mostrar_cantidad2').innerHTML=cantidad_2;
-        
-        }
-        
-        
-
-    });
-    boton_restar_3.addEventListener('click',()=>{
-
-
-        if (cantidad_3===100) {
-
-            cantidad_3=100;
-            
-        }
-        else{
-            cantidad_3-=50;
-            document.querySelector('#mostrar_cantidad3').innerHTML=cantidad_3;
-        
-        }
-        
-
-
-    });
-
-    boton_restar_4.addEventListener('click',()=>{
-
-        if (cantidad_4===100) {
-
-            cantidad_4=100;
-            
-        }
-        else{
-            cantidad_4-=50;
-            document.querySelector('#mostrar_cantidad4').innerHTML=cantidad_4;
-        
-        }       
-        
-
-    });
-
-    boton_restar_5.addEventListener('click',()=>{
-
-        if (cantidad_5===100) {
-
-            cantidad_5=100;
-            
-        }
-        else{
-            cantidad_5-=50;
-            document.querySelector('#mostrar_cantidad5').innerHTML=cantidad_5;
-        
-        }
-        
-        
-
-    });
-*/
     /*fetch('js/frutas.json')
         .then(data=>data.json())
         .then(datos =>{
