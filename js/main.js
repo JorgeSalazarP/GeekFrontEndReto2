9aee6d3b5baabaa2bd480a94331ef92fda60e775
JorgeSalazarP <j-salazarp@hotmail.com>
2020-08-22 11:13:08 +0200

    programado en total pedido

--- js/main.js
+++ js/main.js
@@ -4,50 +4,56 @@
     
     const contenido = document.querySelector('#tarjetas');
     const soltar_productos = document.getElementById('carrito');
+    const pedido = document.getElementById('total_pedido');
+    
     let img_seleccionada="";
     let articulo ="";
     let cantidad_articulo="";
     let precio_articulo="";
     let total_compra=0;
+
     let elemento_seleccionado="";
 
     let frutas = [
         {   
             id: 0,
-            content: 'Kiwi verde al peso (peso aproximado de la unidad 130g).',
+            content: 'Kiwi verde al peso.',
             price: 3.89,
             image:'img/KIWIS.png'
         },
         {
             id: 1,
-            content: 'Melocotón selección al peso (peso aproximado de la unidad 150g).',
+            content: 'Melocotón selección al peso.',
             price: 3.95,
             image:'img/melocotones.png'
         },
         {
             id: 2,
-            content: 'Melón piel de sapo pieza 3,5kg peso aproximado.',
+            content: 'Melón selección al peso.',
             price: 3.47,
             image:'img/melon.png'
         },
         {
             id: 3,
-            content: 'Manzana golden al peso (peso aproximado de la unidad 200g).',
+            content: 'Manzana golden al peso.',
             price: 1.75,
             image:'img/manzanas.png'
         },
         {
             id: 4,
-            content: 'Paraguayo al peso (peso aproximado de la unidad 130g)',
+            content: 'Paraguayo al peso.',
             price: 1.69,
             image:'img/paraguayos.png'
         },
         {
             id: 5,
-            content: 'Sandía cuarto pieza 1,5 kg peso aproximado',
+            content: 'Sandía cuarto pieza al peso.',
             price:0.99,
             image:'img/sandia.png'
         },
+
+
+        
     ];
 
     
@@ -57,7 +63,7 @@
         contenido.innerHTML += `
         <article class="col-md-4 articulo_tarjeta${valor.id}" >
 
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 15rem;">
             <img src=${valor.image} id="tarjeta${valor.id}" class="card-img-top" draggable="true">
             <div class="card-body">
                 <span id="precio_tarjeta${valor.id}">${valor.price}</span><span>€/Kg</span>
@@ -67,7 +73,7 @@
                     <span id="mostrar_cantidad_tarjeta${valor.id}">100</span><span>g</span>
                     <button class="btn" id="sumar_cantidad_${valor.id}">+</button>
                 </div>
-                <p>Arrastrar la imagen al carrito de compra</p>
+                <p>Arrastrar imagen al carrito</p>
             </div>
         </div>
         
@@ -75,6 +81,10 @@
 		`;
       
     }
+
+    soltar_productos.innerHTML=`<i class="fa fa-shopping-cart m-3"></i><span>Carrito</span>`;
+    pedido.innerHTML=`<p class="m-3">Total pedido</p><span id="importe_pedido">0€</span><button class="btn btn-primary btn-lg btn-block">Tramitar pedido</button>`;
+    
     
  
     document.addEventListener('dragstart',(e)=>{
@@ -130,43 +140,57 @@
         e.preventDefault();     
         
         let ultima=elemento_seleccionado[elemento_seleccionado.length-1];
-        console.log(ultima);
-        let crear_articulo_seleccionado = document.createElement("article");
-        crear_articulo_seleccionado.classList.add("articulo_tarjeta"+ultima);
-        soltar_productos.appendChild(crear_articulo_seleccionado);
+        let articulo_seleccionado = document.createElement("article");
+        articulo_seleccionado.classList.add("articulo_tarjeta"+ultima);
+        soltar_productos.appendChild(articulo_seleccionado);
         
         
         
         img_seleccionada.setAttribute("draggable","false");
-        crear_articulo_seleccionado.appendChild(img_seleccionada);
+        articulo_seleccionado.appendChild(img_seleccionada);
         
-        crear_articulo_seleccionado.appendChild(cantidad_articulo);
+        articulo_seleccionado.appendChild(cantidad_articulo);
         let simbolo_multiplicar = document.createElement("span");
         simbolo_multiplicar.innerHTML=" X ";
-        crear_articulo_seleccionado.appendChild(simbolo_multiplicar);
-        crear_articulo_seleccionado.appendChild(precio_articulo);
+        articulo_seleccionado.appendChild(simbolo_multiplicar);
+        articulo_seleccionado.appendChild(precio_articulo);
         let simbolo_igual = document.createElement("span");
         simbolo_igual.innerHTML=" = ";
-        crear_articulo_seleccionado.appendChild(simbolo_igual);
+        articulo_seleccionado.appendChild(simbolo_igual);
 
         let total = document.createElement("span");
-        let operacion=(parseFloat((cantidad_articulo.textContent)/1000)*parseFloat(precio_articulo.textContent)).toFixed(2);
+        let operacion=0;
+        operacion=(parseFloat((cantidad_articulo.textContent)/1000)*parseFloat(precio_articulo.textContent)).toFixed(2);
+        operacion=parseFloat(operacion);
         
-        total.innerHTML=operacion+'€';
-        crear_articulo_seleccionado.appendChild(total);
         total_compra+=operacion;
+       
+       
+
+        let mostrar_suma_pedido=0;
+        mostrar_suma_pedido=total_compra.toFixed(2);
+
+        document.getElementById('importe_pedido').innerHTML=mostrar_suma_pedido+'€';
+      
+        total.innerHTML=operacion+'€';
+        articulo_seleccionado.appendChild(total);
+
         let icono_papelera = document.createElement("span");
-        icono_papelera.innerHTML = `<i id="papelera_${elemento_seleccionado}"class="fa fa-trash"></i>`;
-        crear_articulo_seleccionado.appendChild(icono_papelera);
+        icono_papelera.innerHTML = `<i id="papelera_${elemento_seleccionado}"class="fa fa-trash papelera"></i>`;
+        articulo_seleccionado.appendChild(icono_papelera);
         contenido.removeChild(articulo);
+        
+        
+
+        
+
 
         icono_papelera.addEventListener('click',(e)=>{
 
             const elemento_pulsado = e.target.id;
-            console.log(elemento_pulsado);
+            
       
             let ultima=elemento_pulsado[elemento_pulsado.length-1];
-            console.log(ultima);
             let devolver_producto= document.createElement("article");
             devolver_producto.classList.add("col-md-4");
             devolver_producto.classList.add("articulo_tarjeta"+frutas[ultima].id);
@@ -175,7 +199,7 @@
         
             devolver_producto.innerHTML = `
             
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <img src=${frutas[ultima].image} id="tarjeta${frutas[ultima].id}" class="card-img-top" draggable="true">
                 <div class="card-body">
                     <span id="precio_tarjeta${frutas[ultima].id}">${frutas[ultima].price}</span><span>€/Kg</span>
@@ -185,15 +209,22 @@
                         <span id="mostrar_cantidad_tarjeta${frutas[ultima].id}">100</span><span>g</span>
                         <button class="btn" id="sumar_cantidad_${frutas[ultima].id}">+</button>
                     </div>
-                    <p>Arrastrar la imagen al carrito de compra</p>
+                    <p>Arrastrar la imagen al carrito</p>
                 </div>
             </div>
             
             `;
            
             contenido.appendChild(devolver_producto);
-            soltar_productos.removeChild(crear_articulo_seleccionado);
+            soltar_productos.removeChild(articulo_seleccionado);
 
+
+            total_compra-=operacion;
+            mostrar_suma_pedido=total_compra.toFixed(2);
+
+            if(mostrar_suma_pedido<=0){mostrar_suma_pedido=0;}
+            
+            document.getElementById('importe_pedido').innerHTML=mostrar_suma_pedido+'€';
 
 
 
